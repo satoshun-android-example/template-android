27bccc2d8dcb3a0ca800b4b7de6ca13813a4fa61
SatoShun <shun.sato1@gmail.com>
2024-03-02 12:17:00 +0000

    fix cache

--- feature/home/src/main/java/io/github/satoshun/example/feature/home/HomePresenter.kt
+++ feature/home/src/main/java/io/github/satoshun/example/feature/home/HomePresenter.kt
@@ -1,8 +1,11 @@
 package io.github.satoshun.example.feature.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
@@ -26,8 +29,12 @@ class HomePresenter @AssistedInject internal constructor(
 
   @Composable
   override fun present(): HomeState {
-    val images by produceState<List<Image>?>(initialValue = null) {
-      value = homeRepository.getImages()
+    // TODO: produceState is not working? cuz rememberSaveable is not used
+    var images by rememberSaveable {
+      mutableStateOf<List<Image>?>(null)
+    }
+    LaunchedEffect(Unit) {
+      images = homeRepository.getImages()
     }
 
     return HomeState(
