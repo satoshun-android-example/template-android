d2c72efc3c02edc221c1b6c5840500f90f698218
SatoShun <shun.sato1@gmail.com>
2024-03-02 12:40:53 +0000

    create extension

--- feature/home/src/main/java/io/github/satoshun/example/feature/home/HomePresenter.kt
+++ feature/home/src/main/java/io/github/satoshun/example/feature/home/HomePresenter.kt
@@ -1,11 +1,7 @@
 package io.github.satoshun.example.feature.home
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
@@ -15,6 +11,7 @@ import dagger.assisted.AssistedInject
 import dagger.hilt.components.SingletonComponent
 import io.github.satoshun.example.share.data.HomeRepository
 import io.github.satoshun.example.share.data.Image
+import io.github.satoshun.example.share.ui.produceStateSaveable
 
 class HomePresenter @AssistedInject internal constructor(
   @Assisted private val navigator: Navigator,
@@ -29,12 +26,8 @@ class HomePresenter @AssistedInject internal constructor(
 
   @Composable
   override fun present(): HomeState {
-    // TODO: produceState is not working? cuz rememberSaveable is not used
-    var images by rememberSaveable {
-      mutableStateOf<List<Image>?>(null)
-    }
-    LaunchedEffect(Unit) {
-      images = homeRepository.getImages()
+    val images by produceStateSaveable<List<Image>?>(null) {
+      value = homeRepository.getImages()
     }
 
     return HomeState(
