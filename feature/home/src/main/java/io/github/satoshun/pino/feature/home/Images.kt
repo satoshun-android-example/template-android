a590f15618d7d5da89e43e8bdc75d502a22ef43f
SatoShun <shun.sato1@gmail.com>
2024-03-17 05:50:24 +0000

    add image placeholder

--- feature/home/src/main/java/io/github/satoshun/pino/feature/home/Images.kt
+++ feature/home/src/main/java/io/github/satoshun/pino/feature/home/Images.kt
@@ -1,5 +1,6 @@
 package io.github.satoshun.pino.feature.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,11 +10,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import io.github.satoshun.pino.designsystem.theme.PinoTheme
 import io.github.satoshun.pino.share.data.Image
 
 @Composable
@@ -25,7 +30,7 @@ internal fun Images(
 ) {
   LazyVerticalGrid(
     modifier = modifier.fillMaxSize(),
-    columns = GridCells.Fixed(2),
+    columns = GridCells.Fixed(3),
     contentPadding = contentPadding,
     verticalArrangement = Arrangement.spacedBy(4.dp),
     horizontalArrangement = Arrangement.spacedBy(4.dp),
@@ -35,10 +40,30 @@ internal fun Images(
         modifier = Modifier
           .fillMaxWidth()
           .aspectRatio(1f)
+          .background(MaterialTheme.colorScheme.surfaceVariant)
           .clickable { onImageClick(it) },
         model = it.url,
         contentScale = ContentScale.Crop,
         contentDescription = null,
+      )
+    }
+  }
+}
+
+@Preview
+@Composable
+private fun ImagesPreview() {
+  PinoTheme {
+    Surface {
+      Images(
+        images = listOf(
+          Image(url = "1"),
+          Image(url = "2"),
+          Image(url = "3"),
+          Image(url = "4"),
+        ),
+        contentPadding = PaddingValues(0.dp),
+        onImageClick = {},
       )
     }
   }
