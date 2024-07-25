c066670d49cbafd81b1d63a92a289b73e7fe43e2
SatoShun <shun.sato1@gmail.com>
2024-07-25 00:26:10 +0000

    fix indicator padding

--- feature/home/src/main/java/io/github/satoshun/pino/feature/home/Main.kt
+++ feature/home/src/main/java/io/github/satoshun/pino/feature/home/Main.kt
@@ -6,9 +6,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun Main(
@@ -28,11 +31,22 @@ internal fun Main(
       }
     }
     is HomeTabState.MainState.Success -> {
+      val pullToRefreshState = rememberPullToRefreshState()
       PullToRefreshBox(
         modifier = Modifier.fillMaxSize(),
+        state = pullToRefreshState,
         isRefreshing = state.isRefreshing,
         onRefresh = {
           parentState.eventSink(HomeEvent.Refresh)
+        },
+        indicator = {
+          PullToRefreshDefaults.Indicator(
+            modifier = Modifier
+              .align(Alignment.TopCenter)
+              .padding(top = 96.dp),
+            state = pullToRefreshState,
+            isRefreshing = state.isRefreshing,
+          )
         },
       ) {
         Images(
